9860788498db310f02e52f9072eb11252a5008de
Grzesiek <grekkq97@gmail.com>
2020-03-01 15:10:35 +0100

    inicjalizacja ekranu oled

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,13 @@
 #define LightPin (13)
 #define ButtonPin (14)
 
-// // Screen setup
-// #define SCREEN_WIDTH 128 // OLED display width, in pixels
-// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Screen setup
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-// // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
     PinSetup(LightPin, ButtonPin);
